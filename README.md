861163986bb1e04f0a677f9febaac0ce4e229c2d
edgib102 <58235933+edgib102@users.noreply.github.com>
2021-07-21 03:29:39 +1200

    Got full image generation working
    need to add run.py interaction next

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Reddit to youtube bot
 ### aka my personal money printer
 
+## What it does
+1. Gets post from reddit and puts set number of comments into a list
+2. converts each comment into a transparent image 
 ## Future wants:
 
 - List of terms and categories to filter out to improve comment quality
--- code/Image.py
+++ code/Image.py
@@ -6,11 +6,38 @@ import os.path
 #base text settings. Might change it into a json file if i can be fucked
 FONT_PATH =''
 IMAGE_PATH='Image Bin'
-fontname = "arial.ttf"
+font = "arial.ttf"
 # fontname = "Lato-Regular.ttf"
-fontsize = 11
-colorText = "red"
+fontsize = 55
+colorText = "white"
 
+def split_string(text, maxWords):
+    words = text.split()
+    wc = 0
+    line = ''
+    lines = []
+
+    for word in words:
+        added = False
+        wc += 1
+        line += word + ' '
+        if (wc % maxWords ==0 ):
+            lines.append(line)
+            line = ''
+            added = True
+
+    if (added is False):
+        lines.append(line)
+    
+    return lines
+
+def get_text_size(text_string, font):
+    #returns text size. dont ask me how it works, because i dont know. it just does.
+    ascent, descent = font.getmetrics()
+    text_width = font.getmask(text_string).getbbox()[2]
+    text_height = font.getmask(text_string).getbbox()[3] + descent
+    return (text_width, text_height)
+        
 def construct_image(text,author,name):
     #gets filepath where the image ends up and the name of the image
     filepath = os.path.join(IMAGE_PATH, name)
@@ -18,11 +45,27 @@ def construct_image(text,author,name):
     img = Image.new('RGBA',(1920,1080), (0,0,0,0))
     #opens up the image for editing
     d = ImageDraw.Draw(img)
-    #sets font and font size
-    font = ImageFont.truetype(fontname, 55)
+    #sets fonts and font sizes
+    commentFont = ImageFont.truetype(font, fontsize)
+    authorFont = ImageFont.truetype(font, 30)
     #draws text onto the blank image we created earlier
-    d.text((20,50), text, fill=colorText, font=font)
+    # d.text((20,50), author, fill=colorText, font=authorFont)
+    # d.text((100,200), text, fill=colorText, font=commentFont)
+
+
+    y = 150
+    for line in split_string(text, 10):
+        text_dimensions = get_text_size(line, commentFont)
+        x = (1920 - text_dimensions[0]) / 2
+
+        d.text((x, y),line,font=commentFont,fill=colorText)
+        y += 50
+
     #saves the image ot the set filepath
     img.save(filepath)
+    return()
 
-construct_image('test text','gamerboy','test.png')
+construct_image('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','gamerboy','test.png')
+
+
+
