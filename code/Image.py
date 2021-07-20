58bc6f20aeb98cb2e51b788df623e6a9ab1f5532
edgib102 <58235933+edgib102@users.noreply.github.com>
2021-07-20 23:41:45 +1200

    Cleaned up a ton

--- code/Image.py
+++ code/Image.py
@@ -3,25 +3,26 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 import os.path
 
-# def create_image(text,author,name):
-
+#base text settings. Might change it into a json file if i can be fucked
 FONT_PATH =''
 IMAGE_PATH='Image Bin'
-
 fontname = "arial.ttf"
 # fontname = "Lato-Regular.ttf"
 fontsize = 11
-text = "example@gmail.com"
 colorText = "red"
 
-file = os.path.join(IMAGE_PATH, 'test.png')
+def construct_image(text,author,name):
+    #gets filepath where the image ends up and the name of the image
+    filepath = os.path.join(IMAGE_PATH, name)
+    #creates a blank transparent image
+    img = Image.new('RGBA',(1920,1080), (0,0,0,0))
+    #opens up the image for editing
+    d = ImageDraw.Draw(img)
+    #sets font and font size
+    font = ImageFont.truetype(fontname, 55)
+    #draws text onto the blank image we created earlier
+    d.text((20,50), text, fill=colorText, font=font)
+    #saves the image ot the set filepath
+    img.save(filepath)
 
-# font_file = os.path.join(FONT_PATH, fontname)
-
-
-img = Image.new('RGBA',(1920,1080), (0,0,0,0))
-d = ImageDraw.Draw(img)
-font = ImageFont.truetype(fontname, 55)
-
-d.text((20,50), text, fill=colorText, font=font)
-img.save(file)
+construct_image('test text','gamerboy','test.png')
